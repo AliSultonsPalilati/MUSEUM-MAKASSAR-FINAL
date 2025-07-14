7845a11f2d9b0674a19030709438d6ebd8694147
AliSultonsPalilati <105841102222@student.unismuh.ac.id>
2025-07-14 11:38:20 +0800

    Final2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MUSEUM-MAKASSAR-FINAL-KLMPK6/',
+  base: './',
   build: {
     outDir: 'dist'
   }
