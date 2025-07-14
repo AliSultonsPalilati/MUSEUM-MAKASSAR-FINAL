0304a49af6c153c2256bc50af55d44f51937a097
AliSultonsPalilati <105841102222@student.unismuh.ac.id>
2025-07-14 12:53:55 +0800

    Final4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={closeMenu}>
           Museum Makassar
         </Link>
-        <ul className="nav-menu">
+
+        {/* Hamburger icon */}
+        <div className="menu-toggle" onClick={toggleMenu}>
+          ☰
+        </div>
+
+        <ul className={`nav-menu ${isMobileMenuOpen ? 'active' : ''}`}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">Beranda</Link>
+            <Link to="/" className="nav-links" onClick={closeMenu}>Beranda</Link>
           </li>
           <li className="nav-item">
-            <Link to="/sejarah" className="nav-links">Sejarah</Link>
+            <Link to="/sejarah" className="nav-links" onClick={closeMenu}>Sejarah</Link>
           </li>
           <li className="nav-item">
-            <Link to="/koleksi" className="nav-links">Koleksi</Link>
+            <Link to="/koleksi" className="nav-links" onClick={closeMenu}>Koleksi</Link>
           </li>
           <li className="nav-item">
-            <Link to="/info" className="nav-links">Info</Link>
+            <Link to="/info" className="nav-links" onClick={closeMenu}>Info</Link>
           </li>
           <li className="nav-item">
-            <Link to="/agenda" className="nav-links">Agenda</Link>
+            <Link to="/agenda" className="nav-links" onClick={closeMenu}>Agenda</Link>
           </li>
         </ul>
       </div>
