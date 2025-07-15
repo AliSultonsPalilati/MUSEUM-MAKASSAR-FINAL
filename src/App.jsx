bb7e9e4a16f47f30eceac41b233cce8d61883c78
AliSultonsPalilati <105841102222@student.unismuh.ac.id>
2025-07-15 13:34:42 +0800

    Fitur Scroll Top

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Sejarah from './pages/Sejarah'
-import Koleksi from './pages/Koleksi'
-import Info from './pages/Info'
-import Agenda from './pages/Agenda'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Sejarah from './pages/Sejarah';
+import Koleksi from './pages/Koleksi';
+import Info from './pages/Info';
+import Agenda from './pages/Agenda';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop'; // ⬅️ Tambahkan ini
 
 export default function App() {
   return (
     <>
+      <ScrollToTop /> {/* ⬅️ Tambahkan ini */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -21,5 +23,5 @@ export default function App() {
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
